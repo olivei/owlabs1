b8aa71003035facd385e8497541ebf7544c3d1d0
oliveirf <fransol@sapo.pt>
2015-03-03 14:37:50 -0500

    ok

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,14 @@
+#
+# Cookbook Name:: owlabs1
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+
 remote_file "/tmp/myphoto.jpeg" do
   source "http://photography.nationalgeographic.com/photography/photo-of-the-day/pronking-springbok-karoo-africa/"
   action :nothing
